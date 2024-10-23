c523c8df767bfd7d05381f1a5c63d94c54f0b364
Anton-Wall <Anton.wl.wall@gmail.com>
2024-10-23 09:12:08 +0200

    fghjio

--- övningar/Kladd.c
+++ övningar/Kladd.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+char text[] = "Hello";
+text[5] = '!';
+void main(int argc, char *argv[]){
 
-int main(int argc, char *argv[]){
-    int num1 = atoi(argv[1]);
-    int num2 = atoi(argv[2]);
-    int sum = num1 + num2;
-    printf("%d\n", sum);
-    return sum;
+    printf("%s\n", text);
 };
